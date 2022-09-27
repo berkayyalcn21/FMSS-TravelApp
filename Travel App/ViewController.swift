4e7c2e2aca39f81b25a770b26696647b2d53e25b
Berkay <berkayyalcn1@gmail.com>
2022-09-27 15:08:25 +0300

    Proje load

---
+++ Travel App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Travel App
+//
+//  Created by Berkay on 27.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
