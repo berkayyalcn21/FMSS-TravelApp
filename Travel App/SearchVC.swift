422ef4eaad1500bab74414d3050606763ce15d69
Berkay <berkayyalcn1@gmail.com>
2022-09-28 14:04:31 +0300

    SearchVC design is done

--- Travel App/SearchVC.swift
+++ Travel App/SearchVC.swift
@@ -9,8 +9,28 @@ import UIKit
 
 class SearchVC: UIViewController {
 
+    let searchController = UISearchController()
+    @IBOutlet weak var noDataImageView: UIImageView!
+    @IBOutlet weak var noDataLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupUI()
+    }
+    
+    func setupUI() {
+        searchController.searchResultsUpdater = self
+        navigationItem.searchController = searchController
+        noDataLabel.isHidden = true
+        noDataImageView.isHidden = true
+    }
+}
+
+extension SearchVC: UISearchResultsUpdating {
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        guard let text = searchController.searchBar.text else { return }
+        print(text)
     }
 }
