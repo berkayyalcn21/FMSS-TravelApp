8a920c0ec66cc6d97de7697a7b96074c18220d14
Berkay <berkayyalcn1@gmail.com>
2022-10-08 15:37:01 +0300

    Added Welcome page gif

---
+++ Travel App/Supports/WelcomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeVC.swift
+//  Travel App
+//
+//  Created by Berkay on 8.10.2022.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // First screen gif settings
+        let gif = UIImage.gifImageWithName("fly")
+        imageView.image = gif
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            let story = self.storyboard?.instantiateViewController(identifier: "HomeVC") as! HomeVC
+//            story.modalPresentationStyle = .fullScreen
+//            story.modalTransitionStyle = .crossDissolve
+            self.navigationController?.pushViewController(story, animated: false)
+//            self.show(story, sender: nil)
+        }
+    }
+
+}
